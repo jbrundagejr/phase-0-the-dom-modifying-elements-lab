e308d311c86c487e8b88e8e8c131bd94a6677f57
Jon <jbrundagejr@gmail.com>
2021-03-04 10:43:13 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.getElementById("main").remove()
+const newHeader = document.createElement('h1');
+    document.body.appendChild(newHeader)
+    newHeader.id = ('victory')
+    newHeader.innerHTML = "Jon is the champion"
